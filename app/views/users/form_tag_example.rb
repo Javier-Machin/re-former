2d865ad54768b8daaeaa91767cd57f3411e1f6d3
Javier Machin <javier_machin_acosta@hotmail.com>
2018-11-02 21:11:53 +0000

    update bundle

--- app/views/users/form_tag_example.rb
+++
@@ -1,9 +0,0 @@
-<%= form_tag("/user", method: "post") do %>
-  <%= label_tag(:username, "Username:") %>
-  <%= text_field_tag(:username) %>
-  <%= label_tag(:email, "Email:") %> 
-  <%= text_field_tag(:email) %>
-  <%= label_tag(:password, "Password:") %>  
-  <%= password_field_tag(:password) %>
-  <%= submit_tag("Submit") %>
-<% end %> 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-   resources :users, only: [:new, :create, :edit, :update]
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users, only: [:new, :create, :edit, :update]
 end
